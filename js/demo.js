0ba5d6f145bf2f90566b2684d0b9d3de4a8bdf2a
Miguel <sonegs@hotmail.com>
2020-06-05 18:02:08 +0200

    Exercise of the Module 1

---
+++ js/demo.js
@@ -0,0 +1,7 @@
+/**************************************************************/
+// This document gives the defaults personal details to the form
+/**************************************************************/
+
+document.getElementById("nameForm").value = "Miguel"; // default name 
+document.getElementById("lastNameForm").value = "Cobo Martínez"; // default last name
+document.getElementById("avatar").src = "./img/undraw_profile_pic_ic5t.svg"; // default avatar image
